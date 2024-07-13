9f014799e188327939da54dc611880f738811ee4
Programmer-Anchous <leonovivan171@gmail.com>
2024-07-13 21:14:37 +0300

    cmp auto complete keybindings - improved, <CR> mapping - removed because some conflicts

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -26,7 +26,7 @@ require('mason-lspconfig').setup({
 
 lsp_zero.setup()
 
-
+-- auto completion
 local cmp = require('cmp')
 local luasnip = require('luasnip')
 
@@ -59,24 +59,8 @@ cmp.setup({
         ["<C-j>"] = cmp.mapping.scroll_docs(-4),
         ["<C-k>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-        ['<CR>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                if luasnip.expandable() then
-                    luasnip.expand()
-                else
-                    cmp.confirm({
-                        select = true,
-                    })
-                end
-            else
-                fallback()
-            end
-        end),
-
         ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.locally_jumpable(1) then
+            if luasnip.locally_jumpable(1) then
                 luasnip.jump(1)
             else
                 fallback()
@@ -84,9 +68,7 @@ cmp.setup({
         end, { "i", "s" }),
 
         ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
+            if luasnip.locally_jumpable(-1) then
                 luasnip.jump(-1)
             else
                 fallback()
